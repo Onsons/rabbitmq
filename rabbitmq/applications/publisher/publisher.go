8b4f2e4c4e99809718509c054338bdef65ca7b1f
OnsTliba <tliba.ons@gmail.com>
2020-12-14 10:59:07 +0100

    ok

--- rabbitmq/applications/publisher/publisher.go
+++ rabbitmq/applications/publisher/publisher.go
@@ -20,6 +20,7 @@ func main() {
 	router := httprouter.New()
 	router.POST("/publish/:message", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 		submit(w, r, p)
+		consume()
 	})
 	fmt.Println("Running...")
 	log.Fatal(http.ListenAndServe(":83", router))
@@ -75,4 +76,65 @@ func submit(writer http.ResponseWriter, request *http.Request, p httprouter.Para
 	}
 
 	fmt.Println("publish success!")
+}
+
+func consume() {
+
+	conn, err := amqp.Dial("amqp://" + rabbit_user + ":" + rabbit_password + "@" + rabbit_host + ":" + rabbit_port + "/")
+
+	if err != nil {
+		log.Fatalf("%s: %s", "Failed to connect to RabbitMQ", err)
+	}
+
+	ch, err := conn.Channel()
+
+	if err != nil {
+		log.Fatalf("%s: %s", "Failed to open a channel", err)
+	}
+
+	q, err := ch.QueueDeclare(
+		"publisher2", // name
+		true,         // durable
+		false,        // delete when unused
+		false,        // exclusive
+		false,        // no-wait
+		nil,          // arguments
+	)
+
+	if err != nil {
+		log.Fatalf("%s: %s", "Failed to declare a queue", err)
+	}
+
+	fmt.Println("Channel and Queue established")
+
+	defer conn.Close()
+	defer ch.Close()
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		false,  // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+
+	if err != nil {
+		log.Fatalf("%s: %s", "Failed to register consumer", err)
+	}
+
+	forever := make(chan bool)
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Réduction Prix: %s", d.Body)
+			d.Ack(false)
+		}
+
+	}()
+
+	fmt.Println("Running...")
+	<-forever
+
 }
